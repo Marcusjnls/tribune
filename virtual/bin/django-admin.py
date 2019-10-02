16f2c3dc4375788acfdab6f446a8c1220ceda16b
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-02 10:03:14 +0300

    Heroku deployment

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jean-louis/Desktop/python-apps/the-moringa-tribune/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
